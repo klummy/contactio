04b99f547cc7a4b9011ef05f06e4dc1a8e24d402
A. Yekeen <aayekeen@gmail.com>
2015-10-30 11:34:29 +0100

    Added basic contact model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=20)
+    middle_name = models.CharField(max_length=20, blank=True)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'slug': self.slug})
+
+    def __unicode(self):
+        return self.last_name
+
+    
